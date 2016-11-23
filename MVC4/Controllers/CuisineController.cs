c76acc7bae5a718a7eec7ffdaeff85a85d857988
Andrew Perry <adp2741@gmail.com>
2016-11-23 12:51:01 -0500

    More MVC work

--- MVC4/Controllers/CuisineController.cs
+++ MVC4/Controllers/CuisineController.cs
@@ -8,7 +8,7 @@ namespace MVC4.Controllers
 {
     public class CuisineController : Controller
     {
-        // GET: Cuisine
+        [Authorize]
         public ActionResult Search(string name = "French")
         {
             var message = Server.HtmlEncode(name);
