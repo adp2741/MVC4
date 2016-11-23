065258872ecbc689ab499e241b092f1091f36920
Andrew Perry <adp2741@gmail.com>
2016-11-23 11:12:53 -0500

    Initial commit

---
+++ MVC4/Controllers/CuisineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        public ActionResult Search(string name = "French")
+        {
+            var message = Server.HtmlEncode(name);
+
+            //return Content(string.Format("You were looking up {0}.", message));
+            return File(Server.MapPath("~/Content/site.css"), "text/css");
+        }
+    }
+}
---
+++ MVC4/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MVC4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = String.Format("{0}::{1} {2}", controller, action, id);
+
+            ViewBag.Message = message;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Location = "New York, USA";
+
+            var model = new AboutModel();
+            model.Name = "Andrew";
+            model.Location = "New York, USA";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
