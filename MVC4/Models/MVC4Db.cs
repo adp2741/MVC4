e16430133e855bf6172e86a726049bcfd59aa93b
Andrew Perry <adp2741@gmail.com>
2016-11-25 10:36:02 -0500

    Working on integrating db

---
+++ MVC4/Models/MVC4Db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC4.Models
+{
+    public class MVC4Db : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+    }
+}
