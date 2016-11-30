2b6eb806d4c5bcce3b95e09db41713f2135f5a23
Andrew Perry <adp2741@gmail.com>
2016-11-29 20:57:25 -0500

    Working on Jquery trying to fix page issues

--- MVC4/Controllers/HomeController.cs
+++ MVC4/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 
 namespace MVC4.Controllers
 {
@@ -25,7 +26,7 @@ namespace MVC4.Controllers
             return Json(model, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Index(string searchTerm = null)
+        public ActionResult Index(string searchTerm = null, int page = 1)
         {
             //var model = from r in _db.Restaurants
             //            orderby r.Reviews.Average(review => review.Rating) descending
@@ -42,7 +43,6 @@ namespace MVC4.Controllers
                 _db.Restaurants
                 .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
                 .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
-                .Take(10)
                 .Select(r => new RestaurantListViewModel
                 {
                     Id = r.Id,
@@ -50,7 +50,7 @@ namespace MVC4.Controllers
                     City = r.City,
                     Country = r.Country,
                     CountOfReviews = r.Reviews.Count()
-                });
+                }).ToPagedList(page, 10);
 
             if (Request.IsAjaxRequest())
             {
--- MVC4/Scripts/mvc4.js
+++ MVC4/Scripts/mvc4.js
@@ -12,16 +12,50 @@ $(function () {
 
         $.ajax(options).done(function (data) {
             var $target = $($form.attr("data-mvc4-target"));
-            $target.replaceWith(data);
+            var $newHtml = $(data);
+            $target.replaceWith($newHtml);
+            $newHtml.effect("highlight");
         });
         
         return false;
     };
 
+    var submitAutocompleteForm = function (event, ui) {
+        var $input = $(this);
+        $input.val(ui.item.label);
+
+        var $form = $input.parents("form:first");
+        $form.submit();
+    };
+
     var createAutocomplete = function () {
         var $input = $(this);
-    }
 
-    $("form[data-mvc4-ajax='true']").submit(ajaxForSubmit);
+        var options = {
+            source: $input.attr("data-mvc4-autocomplete"),
+            select: submitAutocompleteForm
+        };
+
+        $input.autocomplete(options);
+    };
+
+    var getPage = function () {
+        var $a = $(this);
+
+        var options = {
+            url: $a.attr("href"),
+            data: $("form").serialize(),
+            type: "get"
+        };
+
+        $.ajax(options).done(function (data) {
+            var target = $a.parents("div.pagedList").attr("data-mvc4-target");
+            $(target).replaceWith(data);
+        });
+        return false;
+    };
+
+    $("form[data-mvc4-ajax='true']").submit(ajaxFormSubmit);
     $("input[data-mvc4-autocomplete").each(createAutocomplete);
+    $(".main-content").on("click", ".pagedList a", getPage);
 });
